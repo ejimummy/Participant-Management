a4dc4ab8cf1192143f0cf9ccdf69a10a66519736
Lisa Hare <hare.lisa@gmail.com>
2018-06-08 17:12:54 -0700

    added Session class properties and methods

--- ParticipantManagement/ParticipantManagement/Session.cs
+++ ParticipantManagement/ParticipantManagement/Session.cs
@@ -8,5 +8,39 @@ namespace ParticipantManagement
 {
     class Session
     {
+        #region Properties
+        /// <summary>
+        /// Scheduled date of workshop
+        /// </summary>
+        public string Date { get; set; }
+        /// <summary>
+        /// Scheduled start time of workshop
+        /// </summary>
+        public string Time { get; set; }
+        /// <summary>
+        /// Scheduled Street Address of session location
+        /// </summary>
+        public string StreetAddress { get; set; }
+        /// <summary>
+        /// City of session location
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// State of session location
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// Zip Code of session location
+        /// </summary>
+        public int ZipCode { get; set; }
+        /// <summary>
+        /// WorkshopId associated from Workshops
+        /// </summary>
+        public int WorkshopId { get; set; }
+
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
